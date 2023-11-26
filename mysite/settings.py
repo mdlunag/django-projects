70fce625de64644360d77b29173874f5e9a9ff60
Maria de Luna <maria.delunag@gmail.com>
2023-11-26 17:53:21 +0000

    [IMP] update and hide secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5czr54(7(ra=jofg60f-&$6-^&n)t91v-3l-g^h0*^kqp55hoh'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
